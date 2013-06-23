815bcdd542c69ab2f204ce48d3e1bd1e4385292e
Roman Lacko <rlacko@ubuntu.(none)>
2013-06-23 22:32:30 +0200

    Fixed setup.py and remove example setting from main configure.zcml

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0'
 setup(name='collective.dexteritycontentmirror',
       version=version,
       description="Dexterity Content Mirror",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       classifiers=[
         "Framework :: Plone",
